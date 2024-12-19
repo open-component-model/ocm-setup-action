f926e2e491bae324af7336b206e0d2b3f4615614
Hilmar Falkenberg <hilmar.falkenberg@sap.com>
2024-12-19 16:39:04 +0100

    test

--- install.sh
+++ install.sh
@@ -2,7 +2,6 @@
 set -o pipefail
 
 REPO=${repo:=open-component-model/ocm}
-: ${WORKSPACE:=/usr/local}
 
 if [ -z "$version" -o "$version" == latest ]; then
   version="$(basename "$(curl -Ls -o /dev/null -w %{url_effective} https://github.com/$REPO/releases/latest)")"
@@ -15,17 +14,12 @@ PLATFORM=linux
 ARCH=amd64
 ARCHIVEFILE="${BINARY}-${VERSION}-${PLATFORM}-${ARCH}.tar.gz"
 URL="https://github.com/$REPO/releases/download/v${VERSION}/$ARCHIVEFILE"
-TARGET=${WORKSPACE}/bin/ocm
 
 cd /tmp
 echo "Installing Open Component Model CLI Tool version $version from $REPO"
 rm -f ocm-cli.tgz
-mkdir -p "$(dirname "$TARGET")"
 curl -Ls -o ocm-cli.tgz "$URL"
 tar --overwrite -xvzf ocm-cli.tgz >/dev/null
-cp ocm "$TARGET"
-chmod a+x "$TARGET"
-echo "ocm installed into $TARGET"
-if [ -n "$GITHUB_OUTPUT" ]; then
-  echo "ocm-path=$TARGET" >> "$GITHUB_OUTPUT"
-fi
+chmod a+x ocm
+
+ocm --version
